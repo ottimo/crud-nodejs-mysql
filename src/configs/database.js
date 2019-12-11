45a4cdfa4b0a6c72a18f0a3459b80305faff225b
ottimo <matteo.rosi@gmail.com>
2019-12-11 14:29:13 +0000

    database production configuration with environment

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,18 @@
 var database = {
+development: {
   host: 'localhost',
   user: 'root',
   password: 'contraseña',
   port: 3306,
   database: 'crudnodejsmysql'
-}
+},
+production: {
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME
+};
 
 
 module.exports = database;
